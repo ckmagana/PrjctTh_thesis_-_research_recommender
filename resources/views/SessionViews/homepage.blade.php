2ade469acee1d28be3421890a5e6620d9f331780
Christian Kent Magana <ckmagana07@gmail.com>
2022-09-13 08:04:05 +0800

    add basic routings and controllers

--- resources/views/SessionViews/homepage.blade.php
+++ resources/views/SessionViews/homepage.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-mmmmm
+
 {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GuestViewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', [GuestViewsController::class, 'guestaccountancy']);
+Route::get('/', [GuestViewsController::class, 'GuestViewsController@guestartsandscience']);
+Route::get('/', [GuestViewsController::class, 'GuestViewsController@guestcomputerstudies']);
+Route::get('/', [GuestViewsController::class, 'GuestViewsController@guestaccountancyeducation']);
+Route::get('/', [GuestViewsController::class, 'GuestViewsController@guestengineering']);
+Route::get('/', [GuestViewsController::class, 'GuestViewsController@guesthotelmanagement']);
+Route::get('/', [GuestViewsController::class, 'GuestViewsController@guestnursing']);
+Route::get('/', [GuestViewsController::class, 'GuestViewsController@guesthomepage']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@accountancy']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@artsandscience']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@computerstudies']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@accountancyeducation']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@engineering']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@hotelmanagement']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@nursing']);
+Route::get('/', [SessionViewsController::class, 'SessionViewsController@homepage']);
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
