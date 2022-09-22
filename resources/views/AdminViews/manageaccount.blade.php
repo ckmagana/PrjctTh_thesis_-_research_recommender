9a5bce5183709971f406758c2cc892ed5a91896d
Christian Kent Magana <ckmagana07@gmail.com>
2022-09-23 00:48:50 +0800

    admin layout manage account page for guidance

--- resources/views/AdminViews/manageaccount.blade.php
+++ resources/views/AdminViews/manageaccount.blade.php
@@ -1,66 +1,7 @@
 @extends('layouts.adminapp')
 
 @section('admincontent')
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="../overview.php">Admin Panel</a>
-    <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-    <!-- Navbar Search-->
 
-    <!-- Navbar-->
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-    <li class="nav-item dropdown">
-
-    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-        <li><a class="dropdown-item" href="#!">Profile</a></li>
-        <li><a class="dropdown-item" href="../adminlogout.php">Logout</a></li>
-    </ul>
-    </li>
-    </ul>
-</nav>
-<div id="layoutSidenav">
-<div id="layoutSidenav_nav">
-<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-<div class="sb-sidenav-menu">
-<div class="nav">
-<a class="nav-link" href="../overview.php">
-<div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-Overview
-</a>
-<div class="sb-sidenav-menu-heading">Manage Section</div>
-<a class="nav-link" href="manageAccount.php">
-<div class="sb-nav-link-icon"><i class="fas fa-book"></i></div>
-Manage Accounts
-</a>
-<a class="nav-link" href="manageDocument.php">
-<div class="sb-nav-link-icon"><i class="fas fa-minus-circle"></i></div>
-Manage Documents
-</a>
-<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#roomcollapse" aria-expanded="false" aria-controls="collapseLayouts">
-
-<div class="sb-nav-link-icon">
-<i class="fas fa-bed"></i>
-</div>
-
-Create new data
-<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-
-</a>
-<div class="collapse" id="roomcollapse" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-<nav class="sb-sidenav-menu-nested nav">
-<a class="nav-link" href="../addNew/addnewstudent.php">Create new Student Account</a>
-<a class="nav-link" href="../addNew/addnewdocument.php">Add new Document</a>
-</nav>
-</div>
-
-<div class="sb-sidenav-footer" style=" position: fixed; bottom: 0; width: 100%;">
-<div class="small" style=" position: fixed; bottom: 0; width: 100%;">Logged in as: Jonh</div>
-</div>
-</nav>
-</div>
-<div id="layoutSidenav_content">
 <main>
 <div class="container-fluid px-4">
 
